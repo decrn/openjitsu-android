226feb0479dcf4ebc7b928342a850876144f6a0b
Denis Carnier <denis_carn@hotmail.com>
2019-01-03 02:22:33 +0100

    Add models

---
+++ app/src/main/java/com/openjitsu/android/openjitsu/models/ExploreItem.kt
@@ -0,0 +1,9 @@
+package com.openjitsu.android.openjitsu.models
+
+interface ExploreItem {
+    val id: String
+    val name: String
+    val description: String
+    val image: String
+    val content: String
+}
---
+++ app/src/main/java/com/openjitsu/android/openjitsu/models/Position.kt
@@ -0,0 +1,50 @@
+package com.openjitsu.android.openjitsu.models
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Position(
+        override val id: String,
+        override val name: String,
+        override val description: String,
+        override val image: String,
+        override val content: String
+//        val related: String[],
+//        val comments: Comment[]
+) : Parcelable, ExploreItem {
+
+    // Parcelable
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()
+//            parcel.readString(),
+//            parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(image)
+        parcel.writeString(content)
+//        parcel.writeString(related)
+//        parcel.writeString(comments)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Position> {
+        override fun createFromParcel(parcel: Parcel): Position {
+            return Position(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Position?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
